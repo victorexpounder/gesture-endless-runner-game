f7aff6d25d01b942545044f8c4b0c36cab9e8c63
Eze Victor Expounder <victorexpounder@gmail.com>
2025-03-02 23:07:16 +0100

    Adding basic choose avatar page

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,8 +19,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com"  />
+        <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400..900&display=swap" rel="stylesheet" />
+      </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        style={{ fontFamily: `"Orbitron", serif`}}
       >
         {children}
       </body>
